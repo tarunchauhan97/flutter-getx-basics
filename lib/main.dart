2fdc193a10dbe2f29dc73f184803a494bd20b795
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-04-06 12:30:29 +0530

    9 make variable rective/observable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/students.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -15,11 +16,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var student = Student();
     return GetMaterialApp(
-      title: 'State Management Reactive',
+      title: 'State Management By Obx and Custom Class',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('State Management Reactive'),
+          title: Text('State Management By Obx and Custom Class'),
         ),
         body: Center(
           child: Column(
@@ -27,15 +29,22 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Obx(() => Text(
-                    "count.value: ${count}",
+                    "Name is : ${student.name.value}",
                     style: TextStyle(fontSize: 25),
                   )),
               SizedBox(height: 10),
               ElevatedButton(
                 onPressed: () {
-                  increment();
+                  student.name.value = student.name.value.toLowerCase();
                 },
-                child: Text("Increment Button"),
+                child: Text("Lower Button"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  student.name.value = student.name.value.toUpperCase();
+                },
+                child: Text("Upper Button"),
               ),
             ],
           ),
---
+++ lib/students.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+class Student {
+  var name = ' Tarun Chauhan'.obs;
+  var age = 21.obs;
+}

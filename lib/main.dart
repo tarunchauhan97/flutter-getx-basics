989ff2a0c173fbf9042ac1dd1c664f667d10059b
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-04-06 12:38:12 +0530

    9 entire class observable

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,17 @@ void main() {
 
 class MyApp extends StatelessWidget {
   //const MyApp({Key? key}) : super(key: key);
-  var count = 0.obs;
-
-  void increment() {
-    count++;
-  }
+  // var count = 0.obs;
+  //
+  // void increment() {
+  //   count++;
+  // }
 
   @override
   Widget build(BuildContext context) {
-    var student = Student();
+    //var student = Student();
+    final student = Student(name: 'Tarun Singh Chauhan', age: 24).obs;
+
     return GetMaterialApp(
       title: 'State Management By Obx and Custom Class',
       home: Scaffold(
@@ -28,21 +30,28 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Obx(() => Text(
-                    "Name is : ${student.name.value}",
-                    style: TextStyle(fontSize: 25),
-                  )),
+              Obx(
+                () => Text(
+                  "Name is : ${student.value.name}",
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
               SizedBox(height: 10),
               ElevatedButton(
                 onPressed: () {
-                  student.name.value = student.name.value.toLowerCase();
+                  //student.value.name = student.value.name.toLowerCase();
+                  student.update((student) {
+                    student?.name = student.name.toString().toLowerCase();
+                  });
                 },
                 child: Text("Lower Button"),
               ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  student.name.value = student.name.value.toUpperCase();
+                  student.update((student) {
+                    student?.name = student.name.toString().toUpperCase();
+                  });
                 },
                 child: Text("Upper Button"),
               ),

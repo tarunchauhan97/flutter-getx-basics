8a6fdc1b33c28dc793e7d3fe5f343a884216f2ab
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-04-06 11:11:29 +0530

    5 Bottom Sheet

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Dialog Box',
+      title: 'Bottom Sheet',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('This is Dialog Area  '),
+          title: Text('This is the Bottom Sheet '),
         ),
         body: Center(
           child: Column(
@@ -24,23 +24,44 @@ class MyApp extends StatelessWidget {
               ElevatedButton(
                 onPressed: () {
                   //Get.snackbar("Its Snack Bar GetX", "Hello SnackBar");
-                  Get.defaultDialog(
-                    title: 'Dialog Title',
-                    titleStyle: TextStyle(fontSize: 18),
-                    middleText: "this will be middle area",
-                    middleTextStyle: TextStyle(fontSize: 19.0),
-                    backgroundColor: Colors.blueGrey,
-                    radius: 80.0,
-                    textCancel: "NO",
-                    cancelTextColor: Colors.white,
-                    textConfirm: "Yes",
-                    confirmTextColor: Colors.white,
-                    onCancel: () {},
-                    onConfirm: () {},
-                    buttonColor: Colors.pinkAccent,
-                  );
+                  // Get.defaultDialog(
+                  //   title: 'Dialog Title',
+                  //   titleStyle: TextStyle(fontSize: 18),
+                  //   middleText: "this will be middle area",
+                  //   middleTextStyle: TextStyle(fontSize: 19.0),
+                  //   backgroundColor: Colors.blueGrey,
+                  //   radius: 80.0,
+                  //   textCancel: "NO",
+                  //   cancelTextColor: Colors.white,
+                  //   textConfirm: "Yes",
+                  //   confirmTextColor: Colors.white,
+                  //   onCancel: () {},
+                  //   onConfirm: () {},
+                  //   buttonColor: Colors.pinkAccent,
+                  // );
+
+                  Get.bottomSheet(Container(
+                    child: Wrap(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.wb_incandescent),
+                          title: Text('Light Theme'),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.light());
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.wb_incandescent_outlined),
+                          title: Text('Dark Theme'),
+                          onTap: () {
+                            Get.changeTheme(ThemeData.dark());
+                          },
+                        ),
+                      ],
+                    ),
+                  ));
                 },
-                child: Text("Show Dialog Button"),
+                child: Text("Bottom Sheet Button"),
               ),
             ],
           ),

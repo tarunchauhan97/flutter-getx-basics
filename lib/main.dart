9611ca55fbfb9bcf6a43826f40808e56c744e84f
tarunchauhan97 <chauhan.tarun897@gmail.com>
2022-04-07 08:20:33 +0530

    15 getview getX controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/controller.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class MyApp extends GetView<ControllerClassCustom> {
   @override
   Widget build(BuildContext context) {
+    Get.put(ControllerClassCustom());
     return GetMaterialApp(
-      title: 'Snack Bar',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('This is snack Bar '),
+          title: Text('Get View'),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              Obx(() => Text(
+                    'The Value is: ${controller.count},',
+                    style: TextStyle(fontSize: 30),
+                  )),
+              SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
-                  Get.snackbar("Its Snack Bar GetX", "Hello SnackBar");
+                  print("controller:${controller.hashCode}");
+                  controller.increment();
                 },
-                child: Text("Show Snack Bar"),
+                child: Text("Incement Button"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  print("controller:${controller.hashCode}");
+                  controller.decrement();
+                },
+                child: Text("Decrement Button"),
               ),
             ],
           ),

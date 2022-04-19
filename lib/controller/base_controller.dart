90f894fce9e75eb3cd670ea65dc6f56bb793d160
tarunchauhan97 <tarun.chauhan@4thpointer.com>
2022-04-19 14:13:55 +0530

    exception handling with get important

---
+++ lib/controller/base_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_getx/helper/dialog_helper.dart';
+import 'package:flutter_getx/services/app_exceptions.dart';
+
+class BaseController {
+  void handleError(error) {
+    hideLoading();
+    if (error is BadRequestException) {
+      var message = error.message;
+      DialogHelper.showErroDialog(description: message);
+    } else if (error is FetchDataException) {
+      var message = error.message;
+      DialogHelper.showErroDialog(description: message);
+    } else if (error is ApiNotRespondingException) {
+      DialogHelper.showErroDialog(description: 'Oops! It took longer to respond.');
+    }
+  }
+
+  showLoading([String? message]) {
+    DialogHelper.showLoading(message);
+  }
+
+  hideLoading() {
+    DialogHelper.hideLoading();
+  }
+}
---
+++ lib/test_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_getx/controller/test_controller.dart';
+
+class TestPage extends StatelessWidget {
+  final controller = TestController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TestPage')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                controller.getData();
+              },
+              child: Text('GET Data', style: TextStyle(fontSize: 24)),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                controller.postData();
+              },
+              child: Text('POST Data', style: TextStyle(fontSize: 24)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

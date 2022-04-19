422129c126be0b32ebe8f51ebddbea0b2335d718
tarunchauhan97 <tarun.chauhan@4thpointer.com>
2022-04-19 14:18:35 +0530

    getx exception handle main file changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/test_page.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -13,59 +14,24 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Bottom Sheet',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('This is the Bottom Sheet '),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  //Get.snackbar("Its Snack Bar GetX", "Hello SnackBar");
-                  // Get.defaultDialog(
-                  //   title: 'Dialog Title',
-                  //   titleStyle: TextStyle(fontSize: 18),
-                  //   middleText: "this will be middle area",
-                  //   middleTextStyle: TextStyle(fontSize: 19.0),
-                  //   backgroundColor: Colors.blueGrey,
-                  //   radius: 80.0,
-                  //   textCancel: "NO",
-                  //   cancelTextColor: Colors.white,
-                  //   textConfirm: "Yes",
-                  //   confirmTextColor: Colors.white,
-                  //   onCancel: () {},
-                  //   onConfirm: () {},
-                  //   buttonColor: Colors.pinkAccent,
-                  // );
-
-                  Get.bottomSheet(Container(
-                    child: Wrap(
-                      children: [
-                        ListTile(
-                          leading: Icon(Icons.wb_incandescent),
-                          title: Text('Light Theme'),
-                          onTap: () {
-                            Get.changeTheme(ThemeData.light());
-                          },
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.wb_incandescent_outlined),
-                          title: Text('Dark Theme'),
-                          onTap: () {
-                            Get.changeTheme(ThemeData.dark());
-                          },
-                        ),
-                      ],
-                    ),
-                  ));
-                },
-                child: Text("Bottom Sheet Button"),
-              ),
-            ],
-          ),
-        ),
+        // appBar: AppBar(
+        //   title: Text('This is the Bottom Sheet '),
+        // ),
+        body: TestPage(),
+        // body: Center(
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     crossAxisAlignment: CrossAxisAlignment.center,
+        //     children: [
+        //       ElevatedButton(
+        //         onPressed: () {
+        //           Get.to(TestPage());
+        //         },
+        //         child: Text("Bottom Sheet Button"),
+        //       ),
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
